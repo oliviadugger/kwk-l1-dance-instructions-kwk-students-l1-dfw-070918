906b96c6d0887516fe7cdda8a86febc9569f2607
oliviadugger <oliviacdugger@gmail.com>
2018-07-15 19:36:49 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,56 @@
 
 # Code your methods below
+
+def Lower_Body
+ 
+  puts "Assume the proper stance"
+
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+
+  puts "Base Footwork"
+
+  puts "Lift right foot"
+
+  puts "Return right foot to the ground"
+
+  puts "Finishing with a **small skip-step** backward"
+end 
+
+def Skip_Step
+
+puts "Skip Step Left"
+
+puts "Lower left foot to the ground"
+puts "Bounce left foot back up slightly, kicking it a few inches back"
+puts "Left, Right, Left and Left"
+
+Skip Step Right
+
+1. Lower right foot to the ground
+2. Bounce right foot back up slightly, kicking it a few inches back
+3. Right, Left, Right and Right
+
+def Upper_Body
+end
+
+def Bob_the_reins
+
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times."
+end
+
+
+def  Lasso
+
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well."
+end
+
+def Bust_A_Move
+  
+  puts "Be cool"
+  puts "Be snazzy!"
+end
+  
